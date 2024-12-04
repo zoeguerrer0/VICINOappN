3ce76fc08837b2d0da19bf8ca6f2b112f08d7fe3
zoeguerrer0 <zoeguerreroepet20@gmail.com>
2024-12-03 21:33:11 -0300

    firebase

--- app.config.js
+++ app.config.js
@@ -1,51 +1,52 @@
 import 'dotenv/config';
 
-export default{
-  "expo": {
-    "name": "VIC",
-    "slug": "VIC",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "myapp",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true
+export default {
+  expo: {
+    name: 'VIC',
+    slug: 'VIC',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'myapp',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.VICINO.app', // Añade tu bundleIdentifier
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
-      }
+    android: {
+      package: 'com.VICINO.app', // Añade tu package name
+      adaptiveIcon: {
+        foregroundImage: './assets/images/adaptive-icon.png',
+        backgroundColor: '#ffffff'
+      },
+      googleServicesFile: './google-services.json', // Añade el archivo de configuración de Google
     },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/images/favicon.png'
     },
-    "plugins": [
-      "expo-router",
+    plugins: [
+      'expo-router',
+      'expo-splash-screen',
       [
-        "expo-splash-screen",
+        'expo-splash-screen',
         {
-          "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff"
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff'
         }
-      ]
+      ],
     ],
-    "experiments": {
-      "typedRoutes": true
-    },
-	extra:{
-		apiKey: process.env.API_KEY,
-		authDomain: process.env.AUTH_DOMAIN,
-		projectID: process.env.PROJECT_ID,
-		storageBucket: process.env.STORAGE_BUCKET,
-		mesagginSenderId: process.env.MESSAGING_SENDER_ID,
-		appId: process.env.APP_ID
-	}
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID
+    }
   }
-}
+};
