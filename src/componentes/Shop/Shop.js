69c1313a9d65495158f6dd6a1a1a7419aad21d17
zoeguerrer0 <zoeguerreroepet20@gmail.com>
2024-12-09 21:28:31 -0300

    update tab

--- src/componentes/Shop/Shop.js
+++ src/componentes/Shop/Shop.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Image, } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image, Platform } from 'react-native';
 import Products from '../Products/Products';
-import {  DataProvider } from '../Context/DataContext';
+import { DataProvider } from '../Context/DataContext';
 import bagIcon from '../../../assets/images/bag.png';
 import accountIcon from '../../../assets/images/account.png';
 import homeIcon from '../../../assets/images/home.png';
 import ModalComponent from '../ModalComponent/ModalComponent';
-
 
 const Shop = ({navigation}) => {
   return (
     <DataProvider>
       <View style={styles.container}>
         <Products />
-		<ModalComponent />
+        <ModalComponent />
       </View>
-	  <View style={styles.navBar}>
-	  <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('Shop')}>
+      <View style={styles.navBar}>
+        <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('Shop')}>
           <Image source={bagIcon} style={styles.icon} />
         </TouchableOpacity>
         <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('Inicio')}>
@@ -34,6 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
+    paddingBottom: Platform.OS === 'ios' ? 100 : 80, // Ajuste para diferentes plataformas
   },
   navBar: {
     flexDirection: 'row',
@@ -41,19 +41,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#6F4E37',
     paddingVertical: 10,
-    position : 'absolute', 
-    bottom: 85, 
+    position: 'absolute', 
+    bottom: 0,  // Asegura que esté en la parte inferior
     left: 0,
     right: 0,
-    borderRadius: 50,
+    borderTopLeftRadius: 20,  // Solo redondea las esquinas superiores
+    borderTopRightRadius: 20, 
+    paddingBottom: Platform.OS === 'ios' ? 20 : 10, // Ajustes para iOS vs Android
+    zIndex: 10,  // Asegura que el tab esté sobre otros elementos
   },
   navButton: {
     alignItems: 'center',
-}, 
-icon: {
-  width: 50, 
-  height: 50,
-},
+  }, 
+  icon: {
+    width: 40, // Puedes usar tamaños más pequeños si deseas
+    height: 40,
+    resizeMode: 'contain', // Asegura que la imagen se ajuste sin deformarse
+  },
 });
 
 export default Shop;
